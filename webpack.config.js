a92caa0f3a3217786053a6ac95c621d5b0d74f19
ctrlxn4 <ctrlxn4@gmail.com>
2018-08-10 19:01:33 +0000

    Adding simple react project
    Adding initial webpack config with babel and react. Supporting running the project locally with `yarn start`.

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const path = require('path');
+
+const plugins = [
+  new HtmlWebPackPlugin({
+    template: "./src/templates/index.html",
+    filename: "./index.html",
+    inject: true,
+  })
+];
+
+const rules = [
+  {
+    test: /\.jsx?$/,
+    exclude: /node_modules/,
+    use: {
+      loader: "babel-loader",
+    }
+  }
+];
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules,
+  },
+  plugins,
+};
