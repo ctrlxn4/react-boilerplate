7a5b832277a1f05900f2fc01977a6f3a4f9efa95
ctrlxn4 <ctrlxn4@gmail.com>
2018-08-11 19:37:05 +0000

    Adding css modules

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-require('./App.css');
+import style from './App.css';
 import React from 'react';
 
 export const App = () => {
-  return <div className="App">
+  return <div className={style.App}>
     <h1>This is my app</h1>
   </div>
 }
-
